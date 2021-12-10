5d77ac34fe48daa1b57c0bbbb1eb4738d0f4501c
enricopic85 <enricodev85@gmail.com>
2021-12-10 15:03:57 +0100

    risoluzione terzo milestone

--- js/script.js
+++ js/script.js
@@ -20,19 +20,30 @@ for(i=1;i<=100;i++){
 
 /*MILESTONE 2:
 Dato un container nel DOM, appendi un elemento html con il numero o la stringa corretta. */
-for(i=1;i<=100;i++){
-    if (i % 3 ===0 && i % 5 === 0 ) {
-        document.querySelector(".container").innerHTML +="<p>FizzBuzz</p> "
-    } else if (i % 3 ===0) {
-        document.querySelector(".container").innerHTML +="<p>Fizz</p> " // console.log("Fizz");
-    } else if (i % 5 === 0) {
-        document.querySelector(".container").innerHTML +="<p>Buzz</p> " // console.log("Buzz");
-    } else{
-        document.querySelector(".container").innerHTML +=`<p>${i}</p>` 
-    }
-    }
+// for(i=1;i<=100;i++){
+//     if (i % 3 ===0 && i % 5 === 0 ) {
+//         document.querySelector(".container").innerHTML +="<p>FizzBuzz</p> "
+//     } else if (i % 3 ===0) {
+//         document.querySelector(".container").innerHTML +="<p>Fizz</p> " // console.log("Fizz");
+//     } else if (i % 5 === 0) {
+//         document.querySelector(".container").innerHTML +="<p>Buzz</p> " // console.log("Buzz");
+//     } else{
+//         document.querySelector(".container").innerHTML +=`<p>${i}</p>` 
+//     }
+//     }
 
 /* MILESTONE 3
 Applica uno stile differente a seconda del valore dell’indice per i multipli di 3, per i multipli di 5 e per i valori che sono sia multipli di 3 che di 5.
 Prima di partire a scrivere codice poniamoci qualche domanda:
 Come faccio a sapere se un numero è divisibile per un altro? Abbiamo visto qualcosa di particolare che possiamo usare? Come creare nuovi elementi html e appenderli al container?*/
+for(i=1;i<=100;i++){
+    if (i % 3 ===0 && i % 5 === 0 ) {
+        document.querySelector(".container-box").innerHTML +='<div class="box-fizzBuzz"><p>fizzBuzz</p></div>';
+    } else if (i % 3 ===0) {
+        document.querySelector(".container-box").innerHTML +=`<div class="box-fizz">fizz</div>`;  // console.log("Fizz");
+    } else if (i % 5 === 0) {
+        document.querySelector(".container-box").innerHTML +=`<div class="box-buzz">buzz</div>`;  // console.log("Buzz");
+    } else{
+        document.querySelector(".container-box").innerHTML +=`<div class="box-empty">${i}</div>`; 
+    }
+    }
