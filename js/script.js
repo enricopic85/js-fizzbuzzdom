52b82d784f3ba7529f84d4907685173a595ba9e5
enricopic85 <enricodev85@gmail.com>
2021-12-10 16:13:28 +0100

    aggiunta let alla i del ciclo

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 //Scrivi un programma che stampi in console i numeri da 1 a 100.
-for(i=0;i<=100;i++){
+for(let i=0;i<=100;i++){
     console.log(i);
 }
 
 /*MILESTONE 1:
 Per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
 Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.*/
-for(i=1;i<=100;i++){
+for(let i=1;i<=100;i++){
    if (i % 3 ===0 && i % 5 === 0 ) {
        console.log("FizzBuzz")
    } else if (i % 3 ===0) {
@@ -36,7 +36,7 @@ Dato un container nel DOM, appendi un elemento html con il numero o la stringa c
 Applica uno stile differente a seconda del valore dell’indice per i multipli di 3, per i multipli di 5 e per i valori che sono sia multipli di 3 che di 5.
 Prima di partire a scrivere codice poniamoci qualche domanda:
 Come faccio a sapere se un numero è divisibile per un altro? Abbiamo visto qualcosa di particolare che possiamo usare? Come creare nuovi elementi html e appenderli al container?*/
-for(i=1; i<=100; i++){
+for(let i=1; i<=100; i++){
     if (i % 3 === 0 && i % 5 === 0 ) {
         document.querySelector(".container-box").innerHTML +='<div class="box box-fizzBuzz">fizzBuzz</div>';
     } else if (i % 3 === 0) {
